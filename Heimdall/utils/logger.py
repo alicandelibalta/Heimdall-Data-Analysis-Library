e0bc27adbeb958d24b6d70a9dcf842031d3b9f37
Alican <alicandelibalta106@gmail.com>
2026-05-23 13:15:22 +0300

    refactor

--- Heimdall/utils/logger.py
+++ Heimdall/utils/logger.py
@@ -4,38 +4,43 @@ import os
 
 def get_logger(name: str = "heimdall") -> logging.Logger:
     """
-    Heimdall kütüphanesi için hem konsola (Stream) hem de dosyaya (File)
-    log yazan esnek ve merkezi logger mekanizması.
+    Heimdall kütüphanesi için projenin kök dizininde otomatik olarak bir 'logs'
+    klasörü açan ve içine logları kaydeden merkezi logger mekanizması.
     """
     logger = logging.getLogger(name)
 
-    # Eğer logger daha önce doldurulmadıysa (çift handler eklenmesini önlemek için)
     if not logger.handlers:
         logger.setLevel(logging.INFO)
 
-        # Logların nasıl görüneceğini belirleyen standart şablonumuz
         formatter = logging.Formatter(
             fmt="%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
 
-        # 1. HANDLER: Konsola Anlık Çıktı Basma (StreamHandler)
+        # 1. HANDLER: Konsol çıktısı
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
-        # 2. HANDLER: Dosyaya Kalıcı Kaydetme (FileHandler)
-        # Log dosyasını projenin o an çalıştığı dizinde 'heimdall.log' adıyla açar
-        log_filename = "heimdall.log"
-        file_handler = logging.FileHandler(log_filename, encoding="utf-8")
+        # --- KLASÖR KORUMA MEKANİZMASI ---
+        # Kodun o an çalıştığı dizinde (Cwd) 'logs' adında bir klasör yolu belirliyoruz
+        log_dir = os.path.join(os.getcwd(), "logs")
+
+        # Eğer bu klasör bilgisayarda yoksa, Python otomatik olarak oluştursun!
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        # Log dosyamızı artık bu güvenli klasörün içine yönlendiriyoruz
+        log_filepath = os.path.join(log_dir, "heimdall.log")
+
+        # 2. HANDLER: Dosya çıktısı
+        file_handler = logging.FileHandler(log_filepath, encoding="utf-8")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
-        # Kütüphane standardı: Harici importlarda hata patlamasını önleyen boş handler
         logger.addHandler(logging.NullHandler())
 
     return logger
 
 
-# Kütüphane içinden doğrudan çağrılacak hazır logger nesnesi
 logger = get_logger()
